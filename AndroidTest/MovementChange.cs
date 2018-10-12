35e3deba5fe3be42729f5b0d38ee8db0f2c2459f
RonTamG <rontam987@gmail.com>
2018-10-12 20:57:24 +0300

    Add project files.

---
+++ AndroidTest/MovementChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AndroidTest
+{
+    [Serializable]
+    class MovementChange
+    {
+        public int Dx { get; set; }
+        public int Dy { get; set; }
+
+        public MovementChange(int dx, int dy)
+        {
+            Dx = dx;
+            Dy = dy;
+        }
+    }
+}
---
+++ AndroidTest/Network.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AndroidTest
+{
+    class Network
+    {
+        public TcpClient TcpClient { get; set; }
+
+        public Network()
+        {
+            TcpClient = new TcpClient();
+            TcpClient.Connect(new IPEndPoint(new IPAddress(new byte[] { 192, 168, 1, 24 }), 5000));
+        }
+
+        public void SendMovement(int dx, int dy)
+        {
+            var Formatter = new BinaryFormatter();
+            MovementChange mv = new MovementChange(dx, dy);
+            Formatter.Serialize(TcpClient.GetStream(), mv);
+        }
+    }
+}
